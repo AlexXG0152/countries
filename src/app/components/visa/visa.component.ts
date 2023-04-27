c5bb7472befe75f47475d53570e4eb4902791930
Alex <al.gr.0017@gmail.com>
2023-04-27 13:00:52 +0300

    feat: html, service logic

--- src/app/components/visa/visa.component.ts
+++ src/app/components/visa/visa.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { VisaService } from 'src/app/services/visa.service';
 
 @Component({
   selector: 'app-visa',
   templateUrl: './visa.component.html',
-  styleUrls: ['./visa.component.scss']
+  styleUrls: ['./visa.component.scss'],
 })
-export class VisaComponent {
+export class VisaComponent implements OnInit {
+  constructor(private visaService: VisaService) {}
+  countriesList?: any = [];
+  visaRequirementsData?: any = [];
 
+  ngOnInit(): void {
+    this.visaService.getCountriesList().subscribe((data) => {
+      this.countriesList = data;
+      console.log(this.countriesList);
+    });
+  }
+
+  getDataByCode() {
+    const code = 'BLR';
+    this.visaService.getDataByCode(code).subscribe((data) => {
+      this.visaRequirementsData = data;
+      console.log(this.visaRequirementsData);
+    });
+  }
 }
