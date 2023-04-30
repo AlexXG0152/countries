9ffd47ca235bf731ecb64c18f6c37d69761b2f27
Alex <al.gr.0017@gmail.com>
2023-04-30 13:51:56 +0300

    refactor: improve result component

--- src/app/components/result/result.component.ts
+++ src/app/components/result/result.component.ts
@@ -1,9 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router, RoutesRecognized } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Country } from 'src/app/interfaces/country.interface';
 import { SearchService } from 'src/app/services/search.service';
 import { NavigationService } from 'src/app/services/navigation.service';
-import { filter, pairwise } from 'rxjs/operators';
 
 @Component({
   selector: 'app-result',
@@ -16,24 +15,25 @@ export class ResultComponent implements OnInit {
     private route: ActivatedRoute,
     private searchService: SearchService,
     private navigation: NavigationService
-  ) {
-    this.router.events
-      .pipe(
-        filter((evt: any) => evt instanceof RoutesRecognized),
-        pairwise()
-      )
-      .subscribe((events: RoutesRecognized[]) => {
-        console.log('previous url', events[0].urlAfterRedirects);
-        console.log('current url', events[1].urlAfterRedirects);
-        this.previousURL = events[0].urlAfterRedirects;
-      });
-  }
+  ) {}
 
   country?: Country;
-  previousURL?: string;
+  // previousURL?: string | null;
 
   ngOnInit(): void {
     const id = this.route.snapshot.params['id'];
+
+    // this.router.events
+    //   .pipe(
+    //     filter((evt: any) => evt instanceof RoutesRecognized),
+    //     pairwise()
+    //   )
+    //   .subscribe((events: RoutesRecognized[]) => {
+    //     console.log('previous url', events[0].urlAfterRedirects);
+    //     console.log('current url', events[1].urlAfterRedirects);
+    //     this.previousURL = events[0].urlAfterRedirects;
+    //   });
+
     if (id.length === 2) {
       this.getCountryInfoByCCA2(id);
     } else {
@@ -44,6 +44,8 @@ export class ResultComponent implements OnInit {
   getCountryInfoByCCA3(id: string) {
     this.searchService.getCountryByCCA3(id).subscribe((data) => {
       this.country = data!;
+      this.giveFullNameForBorders();
+      this.findNativeLanguageFullName();
       this.router.navigate([`/results/${id}`]);
     });
   }
@@ -51,8 +53,33 @@ export class ResultComponent implements OnInit {
   getCountryInfoByCCA2(id: string) {
     this.searchService.getCountryByCCA2(id).subscribe((data) => {
       this.country = data!;
+      this.giveFullNameForBorders();
+      this.findNativeLanguageFullName();
       this.router.navigate([`/results/${id}`]);
     });
+  }
+
+  giveFullNameForBorders() {
+    const bordersFullNameArray: any[] = [];
+    this.searchService.getCountries().subscribe((countries) => {
+      for (const iterator of countries) {
+        if (this.country?.borders?.includes(iterator.cca3)) {
+          bordersFullNameArray.push([iterator.cca3, iterator.name.common]);
+        }
+      }
+      return (this.country!.borders = bordersFullNameArray);
+    });
+  }
+
+  findNativeLanguageFullName() {
+    for (const key in this.country?.languages!) {
+      if (Object.prototype.hasOwnProperty.call(this.country?.languages!, key)) {
+        const element = this.country?.languages![key];
+        if (key === this.country!.nativeLanguage) {
+          this.country!.nativeLanguage = element as unknown as string;
+        }
+      }
+    }
   }
 
   goBack(): Promise<boolean> | undefined {
@@ -71,6 +98,7 @@ export class ResultComponent implements OnInit {
 
     if (
       id === 'lts' ||
+      id === 'isa' ||
       this.navigation.history.at(-1)!.slice(-3) === undefined
     ) {
       return this.router.navigate([`/results`]);
